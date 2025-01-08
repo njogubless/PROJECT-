0e8ed59765855b9bdec66b585d0a89d34634e142
njogubless <pnjogubless@gmail.com>
2025-01-08 21:38:37 +0300

    writing helper functions

--- lib/core/common/styles/login_Signup_widgets/form_divider.dart
+++ lib/core/common/styles/login_Signup_widgets/form_divider.dart
@@ -1,29 +1,30 @@
-// import 'package:devotion/core/constants/colors.dart';
-// import 'package:flutter/material.dart';
+import 'package:devotion/core/constants/colors.dart';
+import 'package:devotion/core/constants/helper_functions.dart';
+import 'package:flutter/material.dart';
 
-// class TFormDivider extends StatelessWidget {
-//   TFormDivider({super.key, required this.dividerText});
+class TFormDivider extends StatelessWidget {
+  const TFormDivider({super.key, required this.dividerText});
 
-//   final String dividerText;
+  final String dividerText;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     //final dark = THelperFunctions.isDarkMode(context);
-//     return Row(
-//       children: [
-//         Flexible(child: Divider(
-//           color: dark ? TColors.darkGrey : TColors.grey,
-//            thickness: 0.5,
-//            indent:60, 
-//            endIndent: 5,
-//         )),
-//         Text(dividerText,style: Theme.of(context).textTheme.labelMedium,),
-//          Flexible(child: Divider(
-//           color: dark ? TColors.darkGrey : TColors.grey,
-//            thickness: 0.5,
-//            indent:60, 
-//            endIndent: 5,)),
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Row(
+      children: [
+        Flexible(child: Divider(
+          color: dark ? TColors.darkGrey : TColors.grey,
+           thickness: 0.5,
+           indent:60, 
+           endIndent: 5,
+        )),
+        Text(dividerText,style: Theme.of(context).textTheme.labelMedium,),
+         Flexible(child: Divider(
+          color: dark ? TColors.darkGrey : TColors.grey,
+           thickness: 0.5,
+           indent:60, 
+           endIndent: 5,)),
+      ],
+    );
+  }
+}
