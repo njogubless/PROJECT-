bd5c1684d2f114bad6ddaa9901a5004c334f7145
njogubless <pnjogubless@gmail.com>
2025-07-17 22:45:33 +0300

    defining other routes for the app

--- lib/config/routes/router.dart
+++ lib/config/routes/router.dart
@@ -1,8 +1,14 @@
-// loggedOut route
+
+
 import 'package:devotion/core/common/navigation/main_layout.dart';
+import 'package:devotion/core/providers/Audio_screen.dart.dart';
+import 'package:devotion/features/Q&A/presentation/screens/question_page.dart';
+import 'package:devotion/features/articles/presentation/screens/article_screen.dart';
+import 'package:devotion/features/audio/presentation/screens/audio_list_page.dart';
 import 'package:devotion/features/auth/presentation/screen/home_screen.dart';
 import 'package:devotion/features/auth/presentation/screen/login_screen.dart';
 import 'package:devotion/features/auth/presentation/screen/sign_up.dart';
+import 'package:devotion/features/books/presentation/screen/book_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -20,12 +26,12 @@ final loggedOutRoute = RouteMap(
 final loggedInRoute = RouteMap(
   routes: {
     '/': (_) => MaterialPage(child: MainLayout()),
-    // '/home': (_) => const MaterialPage(child: HomeScreen()),
-    // '/audio': (_) => const MaterialPage(child: AudioScreen()),
-    // '/devotion': (_) => const MaterialPage(child: DevotionPage()),
-    // '/articles': (_) => MaterialPage(child: ArticleScreen()),
-    // '/books': (_) => MaterialPage(child: BookScreen()),
-    // '/questions': (_) => const MaterialPage(child: QuestionPage()),
+    '/home': (_) => const MaterialPage(child: HomeScreen()),
+    '/audio': (_) => const MaterialPage(child: AudioScreen()),
+    '/devotion': (_) => const MaterialPage(child: DevotionPage()),
+    '/articles': (_) => MaterialPage(child: ArticlePage()),
+    '/books': (_) => MaterialPage(child: BookScreen()),
+    '/questions': (_) => const MaterialPage(child: QuestionPage()),
   },
 );
 
