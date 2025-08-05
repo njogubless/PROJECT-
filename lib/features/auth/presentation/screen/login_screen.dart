717f01987d26b8e9a408efb41f77be7a86c71055
njogubless <pnjogubless@gmail.com>
2025-08-05 13:00:13 +0300

    editing the loginscreen

--- lib/features/auth/presentation/screen/login_screen.dart
+++ lib/features/auth/presentation/screen/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:devotion/core/common/styles/text_strings.dart';
 import 'package:devotion/core/constants/sizes.dart';
 import 'package:devotion/features/auth/controller/auth_controller.dart';
 import 'package:devotion/features/auth/controller/auth_preferences.dart';
+import 'package:devotion/features/auth/presentation/screen/home_screen.dart';
 import 'package:devotion/features/auth/presentation/screen/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
