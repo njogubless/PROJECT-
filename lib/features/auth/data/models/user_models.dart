fc95aff78c018a023636c3db44e9c06de14e3837
njogubless <pnjogubless@gmail.com>
2025-08-05 12:59:01 +0300

    editing the user_model.dart

--- lib/features/auth/data/models/user_models.dart
+++ lib/features/auth/data/models/user_models.dart
@@ -6,6 +6,8 @@ class UserModel {
   final String userName;
   final String userEmail;
   final bool isAdmin;
+  final String? firstName;
+  final String? lastName;
 
   const UserModel({
     required this.uid,
@@ -13,7 +15,30 @@ class UserModel {
     required this.userEmail,
     required this.userName,
     this.isAdmin = false,
+    this.firstName,
+    this.lastName,
   });
+
+  String get displayName {
+    if (firstName != null && firstName!.isNotEmpty) {
+      if (lastName != null && lastName!.isNotEmpty) {
+        return '$firstName $lastName';
+      } else {
+        return firstName!;
+      }
+    } else if (lastName != null && lastName!.isNotEmpty) {
+      return lastName!;
+    } else if (userName.isNotEmpty && userName != 'No name') {
+      return userName;
+    } else {
+   
+      if (userEmail.contains('@')) {
+        return userEmail.split('@')[0];
+      } else {
+        return 'User';
+      }
+    }
+  }
 
   UserModel copyWith({
     String? uid,
@@ -21,6 +46,9 @@ class UserModel {
     String? userName,
     String? userEmail,
     bool? isAdmin,
+    String? firstName,
+    String? lastName,
+    String? phoneNumber,
   }) {
     return UserModel(
       uid: uid ?? this.uid,
@@ -28,6 +56,8 @@ class UserModel {
       userName: userName ?? this.userName,
       userEmail: userEmail ?? this.userEmail,
       isAdmin: isAdmin ?? this.isAdmin,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
     );
   }
 
@@ -38,28 +68,45 @@ class UserModel {
       'userName': userName,
       'userEmail': userEmail,
       'isAdmin': isAdmin,
+      'firstName': firstName,
+      'lastName': lastName,
     };
   }
 
-  // Fixed fromMap method with null safety
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
       uid: (map['uid'] as String?) ?? '',
       isAuthenticated: (map['isAuthenticated'] as bool?) ?? false,
       userName: (map['userName'] as String?) ?? 'No name',
-      userEmail: (map['userEmail'] as String?) ?? 'No email',
+      userEmail: (map['userEmail'] as String?) ??
+          (map['email'] as String?) ??
+          'No email',
       isAdmin: (map['isAdmin'] as bool?) ?? false,
+      firstName: map['firstName'] as String?,
+      lastName: map['lastName'] as String?,
+    );
+  }
+
+
+  factory UserModel.fromFirestore(Map<String, dynamic> firestoreData) {
+    return UserModel(
+      uid: firestoreData['uid'] ?? '',
+      isAuthenticated: true,
+      userEmail: firestoreData['email'] ?? 'No email',
+      userName: firestoreData['displayName'] ?? 'No name',
+      isAdmin: firestoreData['isAdmin'] ?? false,
+      firstName: firestoreData['firstName'],
+      lastName: firestoreData['lastName'],
     );
   }
 
   @override
   String toString() {
-    return 'UserModel(uid: $uid, isAuthenticated: $isAuthenticated, userName: $userName, userEmail: $userEmail, isAdmin: $isAdmin)';
+    return 'UserModel(uid: $uid, isAuthenticated: $isAuthenticated, userName: $userName, userEmail: $userEmail, isAdmin: $isAdmin, firstName: $firstName, lastName: $lastName)';
   }
 
   String toJson() => json.encode(toMap());
 
-  // Fixed fromJson method
   factory UserModel.fromJson(String source) =>
       UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
@@ -71,7 +118,9 @@ class UserModel {
         other.isAuthenticated == isAuthenticated &&
         other.userName == userName &&
         other.userEmail == userEmail &&
-        other.isAdmin == isAdmin;
+        other.isAdmin == isAdmin &&
+        other.firstName == firstName &&
+        other.lastName == lastName;
   }
 
   @override
@@ -80,6 +129,8 @@ class UserModel {
         isAuthenticated.hashCode ^
         userName.hashCode ^
         userEmail.hashCode ^
-        isAdmin.hashCode;
+        isAdmin.hashCode ^
+        (firstName?.hashCode ?? 0) ^
+        (lastName?.hashCode ?? 0);
   }
 }
